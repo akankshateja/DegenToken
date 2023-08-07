b40013dcd8216001bb4e7441dff1e30a1ad2573f
akankshateja <136678468+akankshateja@users.noreply.github.com>
2023-08-08 01:05:25 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,14 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract DegenGamingToken is ERC20, Ownable {
+    constructor() ERC20("Degen", "DGN") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
 
-    constructor() ERC20("Degen", "DGN") {}
+    function mint(address _to, uint256 _amount) external onlyOwner {
+        _mint(_to, _amount);
+    }
+    function transferTokens(address _reciever, uint amount) external{
+            require(balanceOf(msg.sender) >= amount, "you are not owner");
+            approve(msg.sender, amount);
+            transferFrom(msg.sender, _reciever, amount);
+        }
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    function redeem(uint256 _amount) external {
+        require(_amount > 0, "Amount must be greater than 0");
+        require(balanceOf(msg.sender) >= _amount, "Insufficient balance");
+
+        _burn(msg.sender, _amount);
+    }
+
+    function burn(uint256 _amount) external {
+        require(_amount > 0, "Amount must be greater than 0");
+        require(balanceOf(msg.sender) >= _amount, "Insufficient balance");
+
+        _burn(msg.sender, _amount);
     }
 }
